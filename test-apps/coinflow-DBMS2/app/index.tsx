53a48b35d081ecbcad8508ce68e4485fd56238d3
Lihindu Perera <lihindu.indudunu.perera@gmail.com>
2025-10-08 10:32:59 +0530

    coinflow init + NativeWind

---
+++ test-apps/coinflow-DBMS2/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+      <View>
+        <Text className="text-green-500 text-lg">Welcome</Text>
+      </View>
+  );
+}
